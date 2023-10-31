1ec9b9966cdf98a0af266641c808470fe5fe7bd6
cosmonaut <evan@moonside.games>
2023-10-31 15:05:10 -0700

    add Relate to Manipulator

--- src/Rev2/Compatibility/Manipulator.cs
+++ src/Rev2/Compatibility/Manipulator.cs
@@ -8,7 +8,8 @@ public class Manipulator : EntityComponentReader
 	protected void Set<TComponent>(in EntityId entity, in TComponent component) where TComponent : unmanaged => World.Set<TComponent>(entity, component);
 	protected void Remove<TComponent>(in EntityId entity) where TComponent : unmanaged => World.Remove<TComponent>(entity);
 
-	protected void Unrelate<TRelationKind>(in EntityId entityA, in EntityId entityB) where TRelationKind : unmanaged => World.Unrelate<TRelationKind>(entityA, entityB);
-	protected void UnrelateAll<TRelationKind>(in EntityId entity) where TRelationKind : unmanaged => World.UnrelateAll<TRelationKind>(entity);
+	protected void Relate<T>(in EntityId entityA, in EntityId entityB, in T relation) where T : unmanaged => World.Relate(entityA, entityB, relation);
+	protected void Unrelate<T>(in EntityId entityA, in EntityId entityB) where T : unmanaged => World.Unrelate<T>(entityA, entityB);
+	protected void UnrelateAll<T>(in EntityId entity) where T : unmanaged => World.UnrelateAll<T>(entity);
 	protected void Destroy(in EntityId entity) => World.Destroy(entity);
 }
